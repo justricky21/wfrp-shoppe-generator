a891c4c5a24c7a0aedc5185faf5aa0217540b312
ricardoruizfp <rfpimpao@gmail.com>
2020-08-20 07:58:09 +0100

    added main form

---
+++ src/components/MainForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PopulationDropdown from './PopulationDropdown'
+import ResultsForm from './ResultsForm'
+import { meleeWeapons } from '../item_lists/core/core'
+import { rollItemsInArray } from '../util/utils'
+
+
+class MainForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gossipScore: 0,
+            gossipRoll: 0,
+            availableItems: []
+        }
+    }
+   
+    onRollPress = () => {
+        const { gossipScore, gossipRoll } = this.state;
+        const availableItems = rollItemsInArray(meleeWeapons, gossipScore, gossipRoll);
+        this.setState({availableItems});
+    }
+
+    onChange(newValue, stateKey) {
+        this.setState({[stateKey]: newValue})
+    }
+
+    render() {
+        const { availableItems } = this.state;
+        return (
+            <div className='main-form'>
+                <PopulationDropdown/ >
+                <input
+                    placeholder='Input gossip score...'
+                    onChange={e => this.onChange(e.target.value, 'gossipScore')}
+                />
+                <input
+                    placeholder='Input gossip roll...'
+                    onChange={e => this.onChange(e.target.value, 'gossipRoll')}
+                />
+                <button onClick={this.onRollPress}>Roll!</button>
+                <ResultsForm availableItems={availableItems} />
+            </div>
+        )
+    }
+}
+
+export default MainForm
