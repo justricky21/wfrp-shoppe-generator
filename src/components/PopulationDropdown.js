938395bdce77dbb10f60bad1cfa196bf529ccfba
ricardoruizfp <rfpimpao@gmail.com>
2020-08-20 07:57:55 +0100

    added population dropdown

---
+++ src/components/PopulationDropdown.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class PopulationDropdown extends Component {
+    render() {
+        return (
+            <select>
+                <option value='low'>Below 100</option>
+                <option value='average'>Below 1,000</option>
+                <option value='high'>Below 10,000</option>
+                <option value='vhigh'>10,000 or more</option>
+            </select>
+        )
+    }
+}
+
+export default PopulationDropdown
