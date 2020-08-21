e443601e0ff54b4ae5e9a65fe4b8bda6148f2617
ricardoruizfp <rfpimpao@gmail.com>
2020-08-21 06:33:37 +0100

    amended default value on population dropdown

--- src/components/PopulationDropdown.js
+++ src/components/PopulationDropdown.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class PopulationDropdown extends Component {
     render() {
         return (
-            <select>
+            <select defaultValue='average'>
                 <option value='low'>Below 100</option>
-                <option selected value='average'>Below 1,000</option>
+                <option value='average'>Below 1,000</option>
                 <option value='high'>Below 10,000</option>
                 <option value='vhigh'>10,000 or more</option>
             </select>
