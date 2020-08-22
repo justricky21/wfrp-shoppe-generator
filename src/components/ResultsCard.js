d75fe38c373858141a0ccf6693075eb3347d7fe8
ricardoruizfp <rfpimpao@gmail.com>
2020-08-22 06:46:23 +0100

    amended header and main form styling

--- src/components/ResultsCard.js
+++ src/components/ResultsCard.js
@@ -5,7 +5,7 @@ export default function ResultsCard(props) {
   const { key, label, availableGroupItems } = props.group;
   return (
     <div className="results-card">
-      <h1>{label}</h1>
+      <h3>{label}</h3>
       {availableGroupItems.map((item) => {
         return <p key={`${key} ${item.name}`}>{item.name}</p>;
       })}
