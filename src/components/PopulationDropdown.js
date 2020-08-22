83f2b93294123a3cede61332bc60ef386a31338a
ricardoruizfp <rfpimpao@gmail.com>
2020-08-22 05:04:31 +0100

    amended population dropdown

--- src/components/PopulationDropdown.js
+++ src/components/PopulationDropdown.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import './PopulationDropdown.css'
+import FormDropdown from './FormDropdown'
+import { populationBrackets } from '../util/utils'
 
 function PopulationDropdown() {
     return (
-        <select defaultValue='average'>
-            <option value='low'>Below 100</option>
-            <option value='average'>Below 1,000</option>
-            <option value='high'>Below 10,000</option>
-            <option value='vhigh'>10,000 or more</option>
-        </select>
+        <FormDropdown
+            options={populationBrackets}
+            defaultValue='average'
+            label="Select the region's population"
+        />
     )
 }
 
