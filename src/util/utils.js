6401b4abaee9468ffbb6f089b99884f5eaa56edd
ricardoruizfp <rfpimpao@gmail.com>
2020-08-22 05:52:34 +0100

    made core single default export of objects and set util item roll method to care for that

--- src/util/utils.js
+++ src/util/utils.js
@@ -2,67 +2,77 @@
 //     return Math.random() * (1 - 100) + 1;
 // }
 
-export function rollItemsInArray(itemsArray, gossipScore, roll) {
-    const availableItems = [];
-    itemsArray.forEach(item => {
-        switch (item.availability) {
-            case 1:
-                if ((gossipScore - 30 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 2:
-                if ((gossipScore - 20 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 3:
-                if ((gossipScore - 10 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 4:
-                if ((gossipScore >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 5:
-                if ((gossipScore + 10 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 6:
-                if ((gossipScore + 20 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            case 7:
-                if ((gossipScore + 30 >= roll)) {
-                    availableItems.push(item)
-                }
-                break;
-            default:
-                break;
-        }
-    })
-    return availableItems;
+export function rollItemsInArray(itemGroups, gossipScore, roll) {
+  const availableItems = [];
+  itemGroups.forEach((group) => {
+    const availableGroupItems = [];
+    const { label, key, groupItems } = group;
+    groupItems.forEach((item) => {
+      switch (item.availability) {
+        case 1:
+          if (gossipScore - 30 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 2:
+          if (gossipScore - 20 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 3:
+          if (gossipScore - 10 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 4:
+          if (gossipScore >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 5:
+          if (gossipScore + 10 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 6:
+          if (gossipScore + 20 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        case 7:
+          if (gossipScore + 30 >= roll) {
+            availableGroupItems.push(item);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    const filteredGroupObject = {
+      key: key,
+      label: label,
+      availableGroupItems: availableGroupItems,
+    };
+    availableItems.push(filteredGroupObject);
+  });
+  return availableItems;
 }
 
 export const populationBrackets = [
-    {
-        label: 'Below 100',
-        value: 'low'
-    },
-    {
-        label: 'Below 1,000',
-        value: 'average'
-    },
-    {
-        label: 'Below 10,000',
-        value: 'high'
-    },
-    {
-        label: '10,000 or more',
-        value: 'vhigh'
-    }
-]
+  {
+    label: "Below 100",
+    value: "low",
+  },
+  {
+    label: "Below 1,000",
+    value: "average",
+  },
+  {
+    label: "Below 10,000",
+    value: "high",
+  },
+  {
+    label: "10,000 or more",
+    value: "vhigh",
+  },
+];
