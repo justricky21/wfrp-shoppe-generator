85f2435ae31a15121bfa304fe9dfec029b84df9a
ricardoruizfp <rfpimpao@gmail.com>
2020-08-22 05:04:10 +0100

    styled main form

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,51 +1,59 @@
-import React, { Component } from 'react'
-import PopulationDropdown from './PopulationDropdown'
-import './MainForm.css'
-import * as core from '../item_lists/core'
-import { rollItemsInArray } from '../util/utils'
-
+import React, { Component } from "react";
+import PopulationDropdown from "./PopulationDropdown";
+import FormInput from "./FormInput";
+import "./MainForm.css";
+import * as core from "../item_lists/core";
+import { rollItemsInArray } from "../util/utils";
 
 class MainForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gossipScore: 0,
-            gossipRoll: 0,
-        }
-    }
-   
-    onRollPress = () => {
-        const { gossipScore, gossipRoll } = this.state;
-        const { onSetAvailableItems } = this.props
-        const { meleeWeapons } = core;
-        const availableItems = rollItemsInArray(meleeWeapons, gossipScore, gossipRoll);
-        onSetAvailableItems(availableItems);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gossipScore: 0,
+      gossipRoll: 0,
+    };
+  }
 
-    onChange = (stateKey, newState) => {
-        this.setState({[stateKey]: newState})
-    }
+  onRollPress = () => {
+    const { gossipScore, gossipRoll } = this.state;
+    const { onSetAvailableItems } = this.props;
+    const { meleeWeapons } = core;
+    const availableItems = rollItemsInArray(
+      meleeWeapons,
+      gossipScore,
+      gossipRoll
+    );
+    onSetAvailableItems(availableItems);
+  };
 
-    render() {
-        const { onClearPress } = this.props;
-        return (
-            <div className='main-form'>
-                <PopulationDropdown/ >
-                <input
-                    placeholder='Input gossip score...'
-                    type='number'
-                    onChange={e => this.onChange('gossipScore', e.target.valueAsNumber)}
-                />
-                <input
-                    placeholder='Input gossip roll...'
-                    type='number'
-                    onChange={e => this.onChange('gossipRoll', e.target.valueAsNumber)}
-                />
-                <button onClick={this.onRollPress}>Roll!</button>
-                <button onClick={onClearPress}>Clear</button>
-            </div>
-        )
-    }
+  onChange = (stateKey, newState) => {
+    this.setState({ [stateKey]: newState });
+  };
+
+  render() {
+    const { onClearPress, gossipScore, gossipRoll } = this.props;
+    return (
+      <div className="main-form">
+        <PopulationDropdown />
+        <FormInput
+          label="Input gossip score..."
+          stateName="gossipScore"
+          type="number"
+          value={gossipScore}
+          onChange={this.onChange}
+        />
+        <FormInput
+          label="Input gossip roll..."
+          stateName="gossipRoll"
+          type="number"
+          value={gossipRoll}
+          onChange={this.onChange}
+        />
+        <button onClick={this.onRollPress}>Roll!</button>
+        <button onClick={onClearPress}>Clear</button>
+      </div>
+    );
+  }
 }
 
-export default MainForm
+export default MainForm;
